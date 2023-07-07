a4f4ad8c35b04f414e40f15c02d61eae1a3479b3
hudsonandres <123704241+hudsonandres@users.noreply.github.com>
2023-07-07 18:24:46 +0000

    Add files via upload

---
+++ bannedbug1.bat
@@ -0,0 +1,5 @@
+taskkill /f /im steam.exe
+set username=bannedbug1
+reg add "HKCU\Software\Valve\Steam" /v AutoLoginUser /t REG_SZ /d %username% /f
+reg add "HKCU\Software\Valve\Steam" /v RememberPassword /t REG_DWORD /d 1 /f
+start steam://open/main
