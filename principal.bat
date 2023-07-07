363700d7413deed86d0f74c32f5f091cd664d83f
hudsonandres <123704241+hudsonandres@users.noreply.github.com>
2023-07-07 04:12:28 +0000

    Add files via upload

---
+++ principal.bat
@@ -0,0 +1,5 @@
+taskkill /f /im steam.exe
+set username=hudsonandres
+reg add "HKCU\Software\Valve\Steam" /v AutoLoginUser /t REG_SZ /d %username% /f
+reg add "HKCU\Software\Valve\Steam" /v RememberPassword /t REG_DWORD /d 1 /f
+start steam://open/main
