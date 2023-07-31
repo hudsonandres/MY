b96b5280e06971b19fdd4267d49f63703052cb5e
hudsonandres <123704241+hudsonandres@users.noreply.github.com>
2023-07-31 03:43:19 -0300

    Delete bannedbug2.bat

--- bannedbug2.bat
+++
@@ -1,5 +0,0 @@
-taskkill /f /im steam.exe
-set username=bannedbug2
-reg add "HKCU\Software\Valve\Steam" /v AutoLoginUser /t REG_SZ /d %username% /f
-reg add "HKCU\Software\Valve\Steam" /v RememberPassword /t REG_DWORD /d 1 /f
-start steam://open/main
